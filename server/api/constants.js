bfff997c9da8516b3f87033aac5c9d8019324d14
Heorhi Shkliaryk <h.shkliaryk@sam-solutions.com>
2024-03-26 19:06:13 +0100

    use coc proxy

--- server/api/constants.js
+++ server/api/constants.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 export const PORT = process.env.PORT || 3000;
-export const API_SERVICE_URL = 'https://api.clashofclans.com/v1';
+export const API_SERVICE_URL = 'https://cocproxy.royaleapi.dev/v1';
 export const AUTH_TOKEN = process.env.AUTH_TOKEN;
 
 export const CLAN_ID = '%232GUYURGLL';
