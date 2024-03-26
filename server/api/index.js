05876ea1a81d398414321dab97ae21a3b1b18e9e
Heorhi Shkliaryk <h.shkliaryk@sam-solutions.com>
2024-03-26 22:43:27 +0100

    remove cors

--- server/api/index.js
+++ server/api/index.js
@@ -1,20 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
 import { createProxyMiddleware } from 'http-proxy-middleware';
-import cors from 'cors';
 import { PORT, API_SERVICE_URL, AUTH_TOKEN, CLAN_ID } from './constants.js';
-
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200,
-};
 
 // Create Express Server
 const app = express();
-
-// Setting up cors policy
-app.use(cors(corsOptions));
 
 // Logging
 app.use(morgan('dev'));
@@ -30,7 +20,6 @@ app.use(
   createProxyMiddleware({
     headers: {
       Authorization: `Bearer ${AUTH_TOKEN}`,
-      'Access-Control-Allow-Origin': '*',
     },
     target: API_SERVICE_URL,
     changeOrigin: true,
